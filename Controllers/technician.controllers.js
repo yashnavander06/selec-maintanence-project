1591ef1d7e6101c26935159dc8216703375af393
Chinmay Bhole <chinmaymotog5@gmail.com>
2022-08-18 16:03:57 +0530

    added include

--- Controllers/technician.controllers.js
+++ Controllers/technician.controllers.js
@@ -1,5 +1,9 @@
 const upload = require('../Middleware/imageUpload.middleware')
-const { imageModel } = require('../Models/checklist.model')
+const { findUser } = require('../Middleware/checkAuth.middleware')
+const { imageModel } = require("../Models/checklist.model")
+const { User, Role } = require('../Models/users.model')
+const upload = require("../Middleware/imageUpload.middleware");
+const { Ticket } = require("../Models/ticket.models");
 
 
 // const imageUpload = async(req, res) => {
@@ -23,124 +27,113 @@ const { imageModel } = require('../Models/checklist.model')
 // }
 
 //Workorder
-const workOrder =async (req,res)=>{
-    try{
+const workOrder = async(req, res) => {
+    try {
         //checking if the role is technician
-        const username = req.valid.username  // data retrived from token
+        const username = req.valid.username // data retrived from token
         const user = await findUser(username)
-                     
+
         const userid = user._id
         const roles = await Role.find({
-            _id : user.role
-        }) 
-        const techniid = user._id.toString()  
+            _id: user.role
+        })
+        const techniid = user._id.toString()
         const ticketdis = await Ticket.find({})
-        
-        let techid = []
-       for(let i in ticketdis)
-       {
-        techid.push(ticketdis[i].accepted_by)
-       }
-       const filterticket = []
 
-        for(let i in techid)
-        {
-            
-            if(techniid.includes(techid[i]))
-            {               
+        let techid = []
+        for (let i in ticketdis) {
+            techid.push(ticketdis[i].accepted_by)
+        }
+        const filterticket = []
+
+        for (let i in techid) {
+
+            if (techniid.includes(techid[i])) {
                 filterticket.push(ticketdis[i])
             }
-           
+
         }
-        
-        return res.status(200).send({ticket : filterticket , totalcount : filterticket.length})
-            
-    }
-    catch(error){
-return res.status(500).json({msg : error})
+
+        return res.status(200).send({ ticket: filterticket, totalcount: filterticket.length })
+
+    } catch (error) {
+        return res.status(500).json({ msg: error })
     }
 }
 
 //ticket display
 
-const ticketDisplay = async (req,res)=>{
-    try{
-        const username = req.valid.username  // data retrived from token
+const ticketDisplay = async(req, res) => {
+        try {
+            const username = req.valid.username // data retrived from token
+            const user = await findUser(username)
+
+            const userid = user._id
+            const roles = await Role.find({
+                _id: user.role
+            })
+
+
+            const skill = user.skills.toString()
+            const ticketdis = await Ticket.find({})
+
+            let assetid = []
+            for (let i in ticketdis) {
+
+                assetid.push(ticketdis[i].asset_name)
+            }
+
+            const info = []
+            for (let i in assetid) {
+
+
+                if (skill.includes(assetid[i]))
+
+                {
+                    dic = {
+                        subject: ticketdis[i].subject,
+                        assetid: ticketdis[i].asset_name,
+                        description: ticketdis[i].description,
+                        status: ticketdis[i].status,
+                        openat: ticketdis[i].open_at,
+                        location: ticketdis[i].location,
+                        _id: ticketdis[i]._id
+                    }
+
+                    info.push(dic)
+                }
+
+            }
+
+            return res.status(200).send({
+                tickets: info,
+                totalcount: info.length
+            })
+        } catch (error) {
+            return res.status(500).json({ msg: error })
+        }
+    }
+    //ticket accept
+const ticketAccept = async(req, res) => {
+
+    try {
+        const username = req.valid.username // data retrived from token
         const user = await findUser(username)
-                          
+        console.log(user)
         const userid = user._id
-        const roles = await Role.find({
-            _id : user.role
-        })
-        
-        
-        const skill = user.skills.toString()
-        const ticketdis = await Ticket.find({})
-        
-        let assetid = []
-       for(let i in ticketdis)
-       {
-        
-        assetid.push(ticketdis[i].asset_name)
-       }
-       
-       const info = []
-        for(let i in assetid)
-        {
-            
-            
-            if(skill.includes(assetid[i]))
-
-            {   
-                dic = {
-                    subject : ticketdis[i].subject,
-                    assetid : ticketdis[i].asset_name,
-                    description : ticketdis[i].description,
-                    status : ticketdis[i].status,
-                    openat : ticketdis[i].open_at,
-                    location : ticketdis[i].location,
-                    _id : ticketdis[i]._id
-                }    
-                    
-                info.push(dic)
+        const accept = await Ticket.findOneAndUpdate({
+            _id: req.params.id
+        }, {
+            $set: {
+                "accepted": "true",
+                "accepted_by": userid
             }
-            
-        }   
-        
-        return res.status(200).send({
-            tickets : info,totalcount : info.length
-        })
-    }
-    catch(error){
-        return res.status(500).json({msg : error})
+        }, { new: true })
+
+        return res.status(200).send("accepted")
+    } catch (error) {
+        return res.status(500).json({ msg: error })
     }
 }
-//ticket accept
-const ticketAccept = async (req,res) =>{
-        
-          try{
-                const username = req.valid.username  // data retrived from token
-                const user = await findUser(username)
-                console.log(user)                    
-                const userid = user._id
-                    const accept = await Ticket.findOneAndUpdate({
-                        _id : req.params.id 
-                    },
-                    {
-                        $set : {
-                            "accepted" : "true",
-                            "accepted_by" : userid
-                        }
-                    },
-                    {new : true}
-                    )
-                    
-                    return res.status(200).send("accepted")
-            }
 
-    catch(error){
-        return res.status(500).json({msg : error})
-    }   
-}
-
-module.exports = { workOrder,ticketAccept ,ticketDisplay}
+module.exports = { workOrder, ticketAccept, ticketDisplay }
