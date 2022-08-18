17e647a948ddd143f88043e6cf0ed11776100136
Abbas Ahmedabadwala <abbinira2712@gmail.com>
2022-08-18 22:59:14 +0530

    minor changes

--- Routes/technician.routes.js
+++ Routes/technician.routes.js
@@ -6,13 +6,19 @@ const { checkAuth, checkRole } = require('../Middleware/checkAuth.middleware')
 const config = require('../config.json')
 const { upload } = require('../Middleware/imageUpload.middleware')
 
+// Upload Image
 routers.post('/upload', upload.single('image') ,technicianControllers.imageUpload);
 
+// get accepted tickets
 routers.get('/workorder',checkAuth, checkRole(config.ROLE.TECHNICIAN_INTERNAL),technicianControllers.workOrder);
+
+// get tickets according to technician skills
 routers.get('/ticketdisplay',checkAuth, checkRole(config.ROLE.TECHNICIAN_INTERNAL),technicianControllers.ticketDisplay);
+
 // update ticket
 routers.put('/updateTicket/:ticketid',TicketControllers.updateRequesteeTicket)
 
 // close ticket
 routers.patch('/acceptTicket/:ticketid',TicketControllers.updatestatusRequesteeTicket)
+
 module.exports = routers;
